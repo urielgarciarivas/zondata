af7dfb2be1a1202ec00ff1dae249506b0ab59adb
Garz4 <fancygarz4@gmail.com>
2024-09-16 15:08:32 -0600

    Add more tests and update submod

--- test/src/zng_array_test.c
+++ test/src/zng_array_test.c
@@ -26,7 +26,7 @@ TEST(ArrayAllocation, simple_allocation) {
   EXPECT_DIFFERENT(arr, NULL);
   EXPECT_DIFFERENT(arr->data, NULL);
   EXPECT_EQUAL(arr->size, 1);
-  EXPECT_EQUAL(arr->data[0], element);
+  EXPECT_EQUAL(*(arr->data), element);
 
   deallocate_array(arr);
 }
@@ -37,6 +37,7 @@ TEST(ArrayAllocation, preset_allocation) {
   zng_array* arr = allocate_preset_array(size, value);
 
   EXPECT_DIFFERENT(arr, NULL);
+  EXPECT_DIFFERENT(arr->data, NULL);
   EXPECT_EQUAL(arr->size, size);
 
   for (size_t i = 0; i < arr->size; ++i) {
@@ -50,8 +51,8 @@ TEST(ArrayAllocation, empty_allocation) {
   zng_array* arr = allocate_empty_array();
 
   EXPECT_DIFFERENT(arr, NULL);
-  EXPECT_EQUAL(arr->size, 0);
   EXPECT_EQUAL(arr->data, NULL);
+  EXPECT_EQUAL(arr->size, 0);
 
   deallocate_array(arr);
 }
@@ -63,8 +64,8 @@ TEST(ArrayAllocation, copy_allocation) {
   zng_array* arr_copy = allocate_copy_array(arr);
 
   EXPECT_DIFFERENT(arr_copy, NULL);
-  EXPECT_EQUAL(arr_copy->size, size);
-  EXPECT_EQUAL(arr->size, arr_copy->size);
+  EXPECT_DIFFERENT(arr_copy->data, NULL);
+  EXPECT_EQUAL(arr_copy->size, arr->size);
 
   for (size_t i = 0; i < arr->size; ++i) {
     EXPECT_EQUAL(arr->data[i], arr_copy->data[i]);
@@ -74,17 +75,56 @@ TEST(ArrayAllocation, copy_allocation) {
   deallocate_array(arr_copy);
 }
 
-TEST(ArrayVerification, check_if_exists) {
-  zng_array* arr = allocate_preset_array(200, 0);
+TEST(ArrayManipulation, adding_elements) {
+  const int size = 100;
+  zng_array* arr = allocate_empty_array();
 
-  for (int i = 0; i < (int) arr->size; ++i) {
+  for (int i = 0; i < size; ++i) {
+    add_to_array(arr, i);
+  }
+
+  EXPECT_DIFFERENT(arr->data, NULL);
+  EXPECT_EQUAL(arr->size, (size_t) size);
+
+  for (int i = 0; i < size; ++i) {
+    EXPECT_EQUAL(arr->data[i], i);
+  }
+
+  deallocate_array(arr);
+}
+
+TEST(ArrayManipulation, delete_all) {
+  const size_t size = 100;
+  const int value = 59;
+  zng_array* arr = allocate_preset_array(size, value);
+
+  EXPECT_DIFFERENT(arr, NULL);
+  EXPECT_DIFFERENT(arr->data, NULL);
+  EXPECT_EQUAL(arr->size, size);
+
+  delete_all_elements_array(arr);
+
+  EXPECT_DIFFERENT(arr, NULL);
+  EXPECT_EQUAL(arr->data, NULL);
+  EXPECT_EQUAL(arr->size, 0);
+
+  deallocate_array(arr);
+}
+
+TEST(ArrayVerification, check_if_exists) {
+  const int size = 200;
+  zng_array* arr = allocate_preset_array((size_t) size, 0);
+
+  for (int i = 0; i < size; ++i) {
     arr->data[i] = i;
   }
 
   EXPECT_TRUE(exist_in_array(arr, 0));
-  EXPECT_TRUE(exist_in_array(arr, (int) arr->size / 2));
-  EXPECT_TRUE(exist_in_array(arr, (int) arr->size - 1));
-  EXPECT_FALSE(exist_in_array(arr, (int) arr->size));
+  EXPECT_TRUE(exist_in_array(arr, size / 2));
+  EXPECT_TRUE(exist_in_array(arr, size - 1));
+  EXPECT_FALSE(exist_in_array(arr, size));
+  EXPECT_FALSE(exist_in_array(arr, 10000));
+  EXPECT_FALSE(exist_in_array(arr, -10000));
 
   deallocate_array(arr);
 }
@@ -96,35 +136,29 @@ TEST(ArrayVerification, check_equal) {
   EXPECT_TRUE(are_equal_array(arr, arr));
   EXPECT_TRUE(are_equal_array(arr, arr_copy));
   EXPECT_TRUE(are_equal_array(arr_copy, arr_copy));
+
   add_to_array(arr, 1);
   EXPECT_FALSE(are_equal_array(arr, arr_copy));
   add_to_array(arr_copy, 1);
   EXPECT_TRUE(are_equal_array(arr, arr_copy));
+
+  arr->data[10] = 1919;
+  EXPECT_FALSE(are_equal_array(arr, arr_copy));
+  arr_copy->data[10] = 1919;
+  EXPECT_TRUE(are_equal_array(arr, arr_copy));
+
   delete_all_elements_array(arr);
   EXPECT_FALSE(are_equal_array(arr, arr_copy));
   delete_all_elements_array(arr_copy);
   EXPECT_TRUE(are_equal_array(arr, arr_copy));
 
+  add_to_array(arr, 1919);
+  EXPECT_FALSE(are_equal_array(arr, arr_copy));
+  add_to_array(arr_copy, 1919);
+  EXPECT_TRUE(are_equal_array(arr, arr_copy));
+
   deallocate_array(arr);
   deallocate_array(arr_copy);
-}
-
-TEST(ArrayManipulation, adding_elements) {
-  const size_t size = 100;
-  zng_array* arr = allocate_empty_array();
-
-  for (size_t i = 0; i < size; ++i) {
-    add_to_array(arr, (int) i);
-  }
-
-  EXPECT_DIFFERENT(arr->data, NULL);
-  EXPECT_EQUAL(arr->size, size);
-
-  for (size_t i = 0; i < size; ++i) {
-    EXPECT_EQUAL(arr->data[i], (int) i);
-  }
-
-  deallocate_array(arr);
 }
 
 int main(void) {
